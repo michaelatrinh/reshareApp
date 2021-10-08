77e6153a092ce045b6bf7f52ddfba7c1f1aa46ce
michaelBCIT <mtrinh8@my.bcit.ca>
2021-10-08 00:38:08 -0700

    initial commit

---
+++ screens/RestaurantDashboardScreen.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import styled from "styled-components";
+import { initializeApp } from "firebase/app";
+
+
+export default function RestaurantDashboardScreen(){
+    return (
+        <>
+        </>
+    );
+}
