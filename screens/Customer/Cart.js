e09372054299654c9b41e04ff2c9e50a39c751a0
juuheek <jkim844@my.bcit.ca>
2021-11-11 22:32:19 -0800

    Juhee_CartList_First Commit

--- screens/Customer/Cart.js
+++ screens/Customer/Cart.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState, useRef, useContext } from "react";
 import { StyleSheet, Text, View, Pressable, ScrollView } from "react-native";
 import styled from "styled-components/native";
 import { CartContext } from "../../comps/cart";
+import CartList from "../../comps/Customer/CartList";
 
 const ScreenUI = styled.View`
   align-items: center;
@@ -12,19 +13,50 @@ const ScreenUI = styled.View`
 
 const ContainerUI = styled.View`
   background-color: #fff;
-  align-items: flex-start;
-  justify-content: flex-start;
   width: 90%;
   min-height: 100%;
+  display: flex;
+  align-content: space-between;
 `;
 
 const TitleUI = styled.Text`
   font-weight: normal;
   font-family: Poppins;
-  font-size: 22px;
-  margin: 15px 0;
+  font-size: 20px;
+  margin: 15px 0 10px 15px;
 `;
 
+const CartCont = styled.View`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Total = styled.Pressable`
+  width: 95%;
+  height: 37px;
+  border: 0.5px solid #d3cdcd;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+`;
+
+const OrderMore = styled.Pressable`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const OrderMoreText = styled.Text`
+  font-weight: normal;
+  font-family: Poppins;
+  color: #ee9837;
+  font-size: 14px;
+`;
+
+const ButtonCont = styled.View``;
 export default function Cart({ route, navigation }) {
   const { cartTotal, setCartTotal, cart, setCart, addItemToCart } =
     useContext(CartContext);
@@ -34,7 +66,7 @@ export default function Cart({ route, navigation }) {
       <Header navigation={navigation} />
       <ContainerUI>
         <TitleUI>Your Cart</TitleUI>
-        {cart &&
+        {/* {cart &&
           cart.map((item) => (
             <View key={item.name}>
               <Text>{item.name}</Text>
@@ -42,10 +74,20 @@ export default function Cart({ route, navigation }) {
               <Text>{item.expiry}</Text>
               <Text>{item.quantity}</Text>
             </View>
-          ))}
-        <Pressable onPress={() => navigation.goBack(null)}>
-          <Text>Order More</Text>
-        </Pressable>
+          ))} */}
+        <CartCont>
+          <CartList></CartList>
+          <CartList></CartList>
+          <CartList></CartList>
+          <Total>
+            <Text>3 items</Text>
+            <Text>total &11.22</Text>
+          </Total>
+        </CartCont>
+
+        <OrderMore onPress={() => navigation.goBack(null)}>
+          <OrderMoreText>Order More</OrderMoreText>
+        </OrderMore>
 
         <Pressable
           style={{ position: "absolute", bottom: 250 }}
